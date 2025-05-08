625c5cadc04832a10a91a02d741c0ea84ac96e36
Emanuele Czofei <emanuelecz06@gmail.com>
2025-05-08 15:27:44 +0200

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Shop from './pages/Shop';
+import ProductDetails from './pages/ProductDetails';
+import Cart from './pages/Cart';
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+import Logo from '../assets/logo.jpg'
+const Navbar = () => {
+  return (
+    <div>
+      <img src={Logo} />
+    </div>
+  )
+}
+
+export default Navbar
